92d810217facb822ede5426b14f48aceddbeaef7
Ridhan-Github <100027036+Ridhan-Github@users.noreply.github.com>
2022-02-19 19:42:19 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -225,11 +225,11 @@ async def echo(bot, update):
                 "video", format_id, format_ext, randem)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "SVideo",
+                    "Video",
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 InlineKeyboardButton(
-                    "DFile",
+                    "File",
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
@@ -286,11 +286,11 @@ async def echo(bot, update):
             "video", "OFL", "ENON")
         inline_keyboard.append([
             InlineKeyboardButton(
-                "SVideo",
+                "Video",
                 callback_data=(cb_string_video).encode("UTF-8")
             ),
             InlineKeyboardButton(
-                "DFile",
+                "File",
                 callback_data=(cb_string_file).encode("UTF-8")
             )
         ])
